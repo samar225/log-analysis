cc330e97e2f1ac06671308bdc5ebe03b4566d496
snl41_000 <snl.410@hotmail.com>
2018-12-04 16:48:12 +0300

    edit errors

--- log-project.py
+++ log-project.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 import psycopg2
 import datetime
 
@@ -5,14 +6,19 @@ DBNAME = "news"
 
 if __name__ == '__main__':
 
-    conn = psycopg2.connect(database=DBNAME)
+    try:
+        conn = psycopg2.connect(database=DBNAME)
+    except psycopg2.DatabaseError, e:
+        print("<error message>")
 
     curr = conn.cursor()
 
-    curr.execute("SELECT articles.title ," +
-                 " count (path) as sum FROM articles JOIN log" +
-                 " ON  log.path LIKE '%' || articles.slug || '%'" +
-                 " group by title order by sum desc limit 3;")
+    curr.execute("select title, count(*) as views " +
+                 "from log join articles " +
+                 "on log.path = concat('/article/', articles.slug) " +
+                 "group by title " +
+                 "order by views desc" +
+                 " limit 3;")
 
     result = curr.fetchall()
 
@@ -20,18 +26,19 @@ if __name__ == '__main__':
 
     for i in range(len(result)):
 
-        print( '"'+result[i][0]+'"'+ '--' + result[i][1] + 'views')
+        print('"' + str(result[i][0]) + '"' + '--' +
+              str(result[i][1]) + 'views')
 
     print ''
 
-    curr.execute("select authors.name , count (path) as sum " +
-                 "from articles " +
-                 " INNER JOIN log " +
-                 "ON  log.path LIKE '%' || articles.slug || '%' " +
-                 "INNER JOIN authors " +
-                 "on articles.author = authors.id " +
-                 "group by name " +
-                 "order by sum desc limit 3;")
+    curr.execute("select authors.name , count (path) as sum" +
+                 " from articles" +
+                 " INNER JOIN log" +
+                 " on log.path = concat('/article/', articles.slug)" +
+                 " INNER JOIN authors" +
+                 " on articles.author = authors.id" +
+                 " group by name " +
+                 " order by sum desc limit 3;")
 
     result = curr.fetchall()
 
@@ -39,25 +46,26 @@ if __name__ == '__main__':
 
     for i in range(len(result)):
 
-        print result[i][0], '--', result[i][1], 'views'
+        print str(result[i][0]), '--', str(result[i][1]), 'views'
 
     print ''
 
     curr.execute(" SELECT date(time) , " +
-                 " ( COUNT(*)*100.00/(SELECT COUNT(*) FROM log WHERE status  like '404 %') )  AS Percentage " +
+                 "COUNT(status) as b , " +
+                 "(SELECT COUNT(status) FROM log WHERE status" +
+                 "  not like '200 OK') as  a " +
                  "FROM log " +
-                 "where status like '404 %' " +
+                 "where( (select (a/b)*100 from log )  >1.0 ) " +
                  "GROUP BY date(time) , status " +
-                 "order by Percentage desc " +
                  "limit 1;")
 
     result = curr.fetchall()
 
     print("What are the most popular three articles of all time?")
     for i in range(len(result)):
-        d =  result[i][0] 
-        s= d.strftime('%B %d,%Y')
-        e=format(result[i][1] , '.2f')
+        d = result[i][0]
+        s = d.strftime('%B %d,%Y')
+        e = format(result[i][1], '.2f')
 
     for i in range(len(result)):
         t = datetime.datetime.strptime(s, '%B %d,%Y')
@@ -65,7 +73,7 @@ if __name__ == '__main__':
 
     for i in range(len(result)):
 
-        print( dd +'--' + e+ '% errors')
+        print(dd + '--' + e + '% errors')
 
     print ''
 
